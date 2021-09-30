99145837355d1aacb07d6b6c4118ee8dbbda1309
baseb <cmplowman@loyola.edu>
2021-09-30 11:33:49 -0400

    push3

--- .github/lab3.py
+++ .github/lab3.py
@@ -2,11 +2,26 @@
 # Course: CS151, Dr. Rajeev
 # September 30, 2021
 # Lab 3
+import sys
+import math
 package_weight = input("Enter the weight of the package in kg")
 package_weight = float(package_weight)
-travel_distance = input("Enter travel distance in miles")
-travel_distance = float(travel_distance)
 if package_weight > 20 or package_weight < 0:
     print("Invalid package weight")
-elif travel_distance > 3000 or travel_distance < 10:
+    sys.exit()
+travel_distance = input("Enter travel distance in miles")
+travel_distance = float(travel_distance)
+if travel_distance > 3000 or travel_distance < 10:
     print("invalid travel distance")
+    sys.exit
+price = 0
+price = float(price)
+
+if package_weight <= 2:
+    price = 1.10
+    if package_weight > 2 and package_weight <= 6:
+        price = 2.20
+        if package_weight > 6 and package_weight <= 10:
+            price = 3.70
+            if package_weight > 10 and package_weight <= 20:
+                price = 4.80
