44d01c9c5b3663415714901910d2a856b4f7633d
baseb <cmplowman@loyola.edu>
2021-09-30 11:12:31 -0400

    push2

--- .github/lab3.py
+++ .github/lab3.py
@@ -2,3 +2,11 @@
 # Course: CS151, Dr. Rajeev
 # September 30, 2021
 # Lab 3
+package_weight = input("Enter the weight of the package in kg")
+package_weight = float(package_weight)
+travel_distance = input("Enter travel distance in miles")
+travel_distance = float(travel_distance)
+if package_weight > 20 or package_weight < 0:
+    print("Invalid package weight")
+elif travel_distance > 3000 or travel_distance < 10:
+    print("invalid travel distance")
